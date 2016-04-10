985dbaf31050d9cfb7337eadab05f17e6c276767
wenlianglaw <wenlianglaw@gmail.com>
2016-04-10 17:00:18 -0400

    lastest

--- Classes/main.cpp
+++ Classes/main.cpp
@@ -1,8 +1,19 @@
 #include "Board.h"
+#include "Player.h"
+#include "AI_Min_Max.h"
 
-
+#include "Piece.cpp"
+#include "AI_Min_Max.cpp"
 
 
 int main(){
+	Board b;
+	Player p1;
+	p1.genMove(b);
+
+	Piece pc1;
+	pc1.getType();
+	pc1.getPosition();
+	pc1.getPlayer();
 	return 0;
 }
