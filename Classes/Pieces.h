2231895dc161ca0ecdcf8e862006b73d68a2721f
a542545118 <xysware@gmail.com>
2016-04-11 22:12:05 -0400

    move pieces

---
+++ Classes/Pieces.h
@@ -0,0 +1,40 @@
+//
+//  Pieces.h
+//  Animal
+//
+//  Created by yang_xiong on 16/4/9.
+//
+//
+
+#ifndef Pieces_h
+#define Pieces_h
+#include "cocos2d.h"
+#include <iostream>
+
+USING_NS_CC;
+class Pieces : public Node{
+public:
+    enum TypePiece {NIL,RAT,CAT,WOLF,DOG,LEOPARD,TIGER,LION,ELEPHANT};
+private:
+    Point pos; //列，行
+    int player;
+    TypePiece type;
+protected:
+    Sprite* m_sprite;
+    
+public:
+    CREATE_FUNC(Pieces);
+    virtual bool init();
+    void bindSprite(Sprite* sprite);
+    void setProperty(Point p, int play, TypePiece t);
+    TypePiece getType();
+    Point getPositionBlock();
+    void setPositionBlock(Point p);
+    Sprite* getSprite();
+    int getPlayer();
+    void highlight();
+    void changeDirection();
+    void recover();
+    
+};
+#endif /* Pieces_h */
